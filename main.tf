274ecf356bfa4a1b2f7a4a41e16b584c04907bb8
AxelRonny <127845742+AxelRonny@users.noreply.github.com>
2023-06-09 11:29:58 +0200

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,142 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "3.59.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  # Configuration options
+}
+
+provider "azurerm" {
+
+subscription_id = 393e3de3-0900-4b72-8f1b-fb3b1d6b97f1
+tenant_id = 7349d3b2-951f-41be-877e-d8ccd9f3e73c
+client_id = 5706370d-3340-45bd-99d1-16089e0a5f11
+client_secret = Qv58Q~bWv4TwwBQY5T89n9RjDfJb1Z_P3YJHpbbJ
+ skip_provider_registration = true
+   features {}
+}
+
+
+resource "azurerm_resource_group" "webserver" {
+   name = "ar-nginx-server"
+   location = var.location
+}
+# Connect the security group to the network interface
+resource "azurerm_network_interface_security_group_association" "webserver" {
+  network_interface_id      = azurerm_network_interface.webserver.id
+  network_security_group_id = azurerm_network_security_group.allowedports.id
+}
+
+resource "azurerm_network_security_group" "allowedports" {
+   name = "allowedports"
+   resource_group_name = azurerm_resource_group.webserver.name
+   location = azurerm_resource_group.webserver.location
+  
+   security_rule {
+       name = "http"
+       priority = 100
+       direction = "Inbound"
+       access = "Allow"
+       protocol = "Tcp"
+       source_port_range = "*"
+       destination_port_range = "80"
+       source_address_prefix = "*"
+       destination_address_prefix = "*"
+   }
+
+   security_rule {
+       name = "https"
+       priority = 200
+       direction = "Inbound"
+       access = "Allow"
+       protocol = "Tcp"
+       source_port_range = "*"
+       destination_port_range = "443"
+       source_address_prefix = "*"
+       destination_address_prefix = "*"
+   }
+
+   security_rule {
+       name = "ssh"
+       priority = 300
+       direction = "Inbound"
+       access = "Allow"
+       protocol = "Tcp"
+       source_port_range = "*"
+       destination_port_range = "22"
+       source_address_prefix = "*"
+       destination_address_prefix = "*"
+   }
+}
+
+resource "azurerm_public_ip" "webserver_public_ip" {
+   name = "webserver_public_ip"
+   location = var.location
+   resource_group_name = azurerm_resource_group.webserver.name
+   allocation_method = "Dynamic"
+
+   tags = {
+       environment = var.environment
+       costcenter = "it"
+   }
+
+   depends_on = [azurerm_resource_group.webserver]
+}
+
+resource "azurerm_network_interface" "webserver" {
+   name = "nginx-interface"
+   location = azurerm_resource_group.webserver.location
+   resource_group_name = azurerm_resource_group.webserver.name
+
+   ip_configuration {
+       name = "internal"
+       private_ip_address_allocation = "Dynamic"
+       subnet_id = azurerm_subnet.webserver-subnet.id
+       public_ip_address_id = azurerm_public_ip.webserver_public_ip.id
+   }
+
+   depends_on = [azurerm_resource_group.webserver]
+}
+
+resource "azurerm_linux_virtual_machine" "nginx" {
+   size = var.instance_size
+   name = "nginx-webserver"
+   resource_group_name = azurerm_resource_group.webserver.name
+   location = azurerm_resource_group.webserver.location
+   custom_data = base64encode(file("init-script.sh"))
+   network_interface_ids = [
+       azurerm_network_interface.webserver.id,
+   ]
+
+   source_image_reference {
+       publisher = "Canonical"
+       offer = "UbuntuServer"
+       sku = "18.04-LTS"
+       version = "latest"
+   }
+
+   computer_name = "nginx"
+   admin_username = "adminuserar"
+   admin_password = "ronnyand2002@yahoo.fr"
+   disable_password_authentication = false
+
+   os_disk {
+       name = "nginxdisk01"
+       caching = "ReadWrite"
+       #create_option = "FromImage"
+       storage_account_type = "Standard_LRS"
+   }
+
+   tags = {
+       environment = var.environment
+       costcenter = "it"
+   }
+
+   depends_on = [azurerm_resource_group.webserver]
+}
+
